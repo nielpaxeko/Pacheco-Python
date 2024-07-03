d0fe754777b40109879e3e5ab1f0d8ac3919f941
Edgar Pacheco <nielpaxeko@gmail.com>
2024-07-03 12:18:44 -0400

    Added Day 12

---
+++ Beginner/Day10/notes.py
@@ -0,0 +1,26 @@
+# Day 10 Notes - FUNCTIONS WITH OUTPUTS
+
+# A. FUNCTIONS WITH OUTPUTS
+def format_name1(first_name, last_name):
+    return f"{first_name.title()} {last_name.title()}"
+print(format_name1("MATThew", "aDrianus"))
+
+
+# B. MULTIPLE RETURN VALUES
+def format_name2(first_name, last_name):
+    if first_name == "" or last_name == "":
+        return "Wrong input!"
+    return f"{first_name.title()} {last_name.title()}"
+
+# C. DOCSTRINGS -> documenting a function
+def format_name3(first_name, last_name):
+    """Take a first and last name and join them together in title case"""
+    if first_name == "" or last_name == "":
+        return "Wrong input!"
+    return f"{first_name.title()} {last_name.title()}"
+
+print(format_name3("MATThew", "aDrianus"))
+
+# D. PRINT VS RETURN
+# Remember that print only shows message in console while return can be used in a function to return a variable back to the system.
+
