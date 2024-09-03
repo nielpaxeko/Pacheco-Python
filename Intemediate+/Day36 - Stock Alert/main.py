dbf52c88a73a0de2f06090408249416e4364ca4b
Edgar Pacheco <nielpaxeko@gmail.com>
2024-09-03 13:34:34 -0400

    Finished Day 36

--- Intemediate+/Day36 - Stock Alert/main.py
+++ Intemediate+/Day36 - Stock Alert/main.py
@@ -23,39 +23,57 @@ AV_PARAMS = {
 }
 
 # Get close values
-# response = requests.get(AV_URL, params=AV_PARAMS)
-# av_data = response.json()
+response = requests.get(AV_URL, params=AV_PARAMS)
+av_data = response.json()
 
-# ayer_close = round(float(list(av_data["Time Series (Daily)"].values())[0]["4. close"]))
-# anteayer_close = round(float(list(av_data["Time Series (Daily)"].values())[1]["4. close"]))
-# print(ayer_close)
-# print(anteayer_close)
+ayer_close = round(float(list(av_data["Time Series (Daily)"].values())[0]["4. close"]))
+anteayer_close = round(float(list(av_data["Time Series (Daily)"].values())[1]["4. close"]))
+print(ayer_close)
+print(anteayer_close)
+
+# Check for major change
+major_change = False
+difference = round(((ayer_close - anteayer_close) * 100) / anteayer_close)
+print(f"Difference: {difference}")
+if (abs(difference) > 5):
+    major_change = True
+
+if major_change:
+    NEWS_URL = "https://newsapi.org/v2/everything"
+    NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+    NEWS_PARAMS = {
+        "q": COMPANY_NAME,
+        "searchIn": "description",
+        "language": "en",
+        "pageSize": 3,
+        "apiKey": NEWS_API_KEY,
+    }
 
 
+    news_request = requests.get(NEWS_URL, params=NEWS_PARAMS)
+    news_request.raise_for_status()
+    news_data = news_request.json()
 
-# def check_major_change(ayer, anteayer):
-#     difference = ((ayer - anteayer) * 100)
-#     if (difference>5):
-#         return True
-#     else:
-#         return False
+    # Send email reporting major change and latest news on the company
+    for i in range(3):
+        company = f"{STOCK}: {str(difference)}%"
+        headline = "Headline: " + news_data["articles"][i]["title"]
+        desc = "Brief: " + news_data["articles"][i]["description"]
+        message = f"Subject: Stock Update: {STOCK}\n\n{company}\n{headline}\n{desc}"
+        try:
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user = MY_EMAIL, password = PASSWORD)
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs="edpvpro@hotmail.com",
+                    msg= message
+                )
+                print("Message sent successfully")
+        except Exception as e:
+            print(f"An error occurred while sending the email: {e}")
+        
     
-# check_major_change(ayer_close, anteayer_close)
-NEWS_URL = "https://newsapi.org/v2/everything"
-NEWS_API_KEY = os.getenv("NEWS_API_KEY")
-NEWS_PARAMS = {
-    "q": COMPANY_NAME,
-    "searchIn": "description",
-    "language": "en",
-    "pageSize": 3,
-    "apiKey": NEWS_API_KEY,
-}
-
-news_request = requests.get(NEWS_URL, params=NEWS_PARAMS)
-news_request.raise_for_status()
-news_data = news_request.json()
-print(news_data)
-
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
