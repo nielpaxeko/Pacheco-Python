6f4b6a726fe5f5f0aa51bbe20e33bedb3a7c84a8
Edgar Pacheco <nielpaxeko@gmail.com>
2025-03-07 13:38:09 -0500

    Added contact functionality to blog

--- Intermediate - Upper/Capstone Project - Blog/server.py
+++ Intermediate - Upper/Capstone Project - Blog/server.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 from post import Post
+import smtplib
+from dotenv import load_dotenv
+import os
+# Load environment variables from the .env file
+load_dotenv()
+MY_EMAIL = os.getenv("MY_EMAIL")
+PASSWORD =  os.getenv("PASSWORD")
 
 with open("blog_data.json", "r") as file:
-        posts = json.load(file)
-        post_objects = []
-        for post in posts:
-            post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-            post_objects.append(post_obj)
+    posts = json.load(file)
+    post_objects = []
+    for post in posts:
+        post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+        post_objects.append(post_obj)
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def get_all_posts():
     return render_template("index.html", blog_data=post_objects)
 
@@ -21,17 +29,38 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "GET":
+        return render_template("contact.html", msg_sent=False)
+    else:
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+        send_email(name, email, phone, message)
+        return render_template("contact.html", msg_sent=True)
+    
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg= f"Subject: Testing\n\n{email_message}"
+        )
 
-@app.route('/post/<int:post_id>')
+
+@app.route("/post/<int:post_id>")
 def post(post_id):
     requested_post = None
     for blog_post in post_objects:
         if blog_post.id == post_id:
             requested_post = blog_post
-    return render_template("post.html", post = requested_post )
+    return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
