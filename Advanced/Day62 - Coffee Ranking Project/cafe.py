ec5ea1c2596ef1a10d1b2f203abc10f7e2f14b49
Edgar Pacheco <nielpaxeko@gmail.com>
2025-03-11 15:24:04 -0400

    added day 62 coffee rating form

---
+++ Advanced/Day62 - Coffee Ranking Project/cafe.py
@@ -0,0 +1,10 @@
+class Cafe():
+    def __init__(self, row):
+        self.name = row[0]
+        self.location = row[1]
+        self.open = row[2]
+        self.close = row[3]
+        self.coffee = row[4]
+        self.wifi = row[5]
+        self.power = row[6]
+        
